5a39bf54e9dc5aa0f485ba5b4dd74868833b129a
lee <yeluowoxin@sina.com>
2019-04-15 11:12:42 +0800

    process string join

---
+++ golang知识点/string_join.go
@@ -0,0 +1,44 @@
+//字符串的链接方式
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+)
+
+//使用+号拼接
+func AddStringWithOperator() string{
+	hello := "hello"
+	world := "world"
+	result := hello + world
+	return result
+}
+//使用fmt.Sprintf拼接，包含数字之类的时候使用此方法
+func ConnectStringWithNum(first string, last int) string{
+	first = first
+	last = last
+	result := first + fmt.Sprintf("d%", last)
+	return result
+}
+
+//string.Join
+func JoinString(first, last string) string{
+	first = first
+	last = last
+	return strings.Join([]string{first, last}, "")
+}
+
+//buffer.WriteString()
+func ConnectStringWithBuffer(first, last string) string{
+	var buffer bytes.Buffer
+	buffer.WriteString(first)
+	buffer.WriteString(last)
+	return buffer.String()
+}
+func main() {
+	name := "lee"
+	age := 34
+	result := ConnectStringWithNum(name, age)
+	fmt.Println(result)
+}
