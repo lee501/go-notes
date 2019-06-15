0d1a735f40045465c6602dc82ba531ec569d9514
lee <yeluowoxin@sina.com>
2019-06-15 20:38:56 +0800

    add ignore

---
+++ golang知识点/anonymous_struct.go
@@ -0,0 +1,63 @@
+//匿名字段： 只提供类型，而不写字段名的方式
+
+//struct中，无论使用的是指针的方式声明还是普通方式，访问其成员都使用"."，
+//在访问的时候编译器会自动把 stu2.name 转为 (*stu2).name。
+
+// 当匿名字段是一个结构体的时候，
+// 那么这个结构体所拥有的全部字段都被隐式地引入了当前定义的这个结构体，
+package main
+
+import "go/ast"
+
+//人
+type Person struct {
+	name string
+	sex  byte
+	age  int
+}
+//学生
+type Student struct {
+	Person // 匿名字段，那么默认Student就包含了Person的所有字段
+	id     int
+	addr   string
+	name string // 同名字段
+}
+
+//非结构体匿名字段
+//所有的内置类型和自定义类型都是可以作为匿名字段的
+type mystr string //自定义类型
+type Teacher struct {
+	int   //匿名字段，内置类型
+	mystr //匿名字段，自定义类型
+}
+
+//结构体指针
+//注意成员使用.访问的时候，go是转换成指针后访问的： stu.name 转换为(*stu).name
+type Student1 struct {
+	*Person
+	id int
+}
+func main() {
+	//初始化
+	s1 := Student{Person{"mike", 'm', 18}, 1, "sz", "ss"} //顺序初始化，注意Person的初始化方法
+
+	s2 := Student{Person: Person{name: "lee"}, id: 1}  //部分成员初识化
+
+	//成员操作
+	var s3 Student
+	s3.name = "lee" //同名字段默认给外层成员赋值
+	s3.Person.name = "anne" //同名字段给内层成员赋值
+	s3.sex = 0 //等同于s3.Person.sex
+	//Person为结构体，是一个匿名字段
+	s3.Person = Person{"lee", 1, 22}
+
+	//非结构体匿名字段的赋值
+	t := Teacher{int: 1, mystr: "hello"}
+
+	//	结构体指针的初始化
+	s4 := Student1{&Person{"mike", 1, 18}, 1}
+	// 声明变量方式
+	var s5 Student1
+	s5.Person = new(Person) //new 一个空间
+	s5.Person.name = "lee" //等同于s5.name
+}
--- main.go
+++ main.go
@@ -1,9 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
+type SplitFunc func(data string) (result []string)
+
+type Mystruct struct{
+	split SplitFunc
+	Iscalled bool
+}
 func main()  {
-	s := "浙江省杭州市"
-	storune := []rune(s)
-	fmt.Println(string(storune[:2]))
+	//s := "浙江省杭州市"
+	//storune := []rune(s)
+	//fmt.Println(string(storune[:2]))
+	//input := bufio.NewScanner(os.Stdin)
+	a := false
+	suffer := NewSuffer(a)
+	suffer.Callback()
+}
+
+func NewMystruct(called bool) *Mystruct {
+	return &Mystruct{
+		split: SplitString,
+		Iscalled: called,
+	}
+}
+
+func SplitString(data string) (result []string){
+	return strings.Split(data, ",")
+}
+
+func (m *Mystruct) Split(split SplitFunc) {
+	if m.Iscalled {
+		panic("Split called after Scan")
+	}
+	m.split = split
+}
+
+type Suffer struct {
+	check bool
+}
+
+func (s *Suffer) Callback() {
+	if s.check {
+		panic("test the next action")
+	}
+	fmt.Println(111)
+}
+
+func NewSuffer(a bool) *Suffer {
+	return &Suffer{
+		check: a,
+	}
 }
