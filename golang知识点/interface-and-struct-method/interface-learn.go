8e5465872d10648c9f9474533ff7d72212e6de38
lee <yeluowoxin@sina.com>
2019-07-03 00:03:20 +0800

    interface nil and slice use key: value

---
+++ golang知识点/interface-and-struct-method/interface-learn.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+//interface与nil判等的问题
+//interface的内部包含type和value
+//interface==nil， 仅当type和value均为nil（nil, nil）
+
+//检查以下题目，type有值，value==nil
+type Animals interface {
+	Meow()
+}
+
+type Catty struct {
+
+}
+
+func (*Catty) Meow() {
+	fmt.Println("meow")
+}
+
+func GetAnimal() Animals {
+	var catty  *Catty
+	catty = &Catty{}
+	return catty
+}
+
+func main() {
+	var cat = GetAnimal()
+	m := reflect.TypeOf(cat)
+	fmt.Println(m)
+	kind := m.Kind()
+	fmt.Println(kind)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
