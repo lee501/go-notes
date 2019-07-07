24710013719e1cc45912ac5231412a9ba00cac7d
lee <yeluowoxin@sina.com>
2019-07-07 16:22:04 +0800

    copy file

---
+++ golang-knowledge-point/senior/copyfile.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"io"
+	"os"
+)
+
+func CopyFile(toFile, fromFile string) (n int64, err error) {
+	fromfile, err := os.Open(fromFile)
+	if err != nil {
+		return
+	}
+	defer fromfile.Close()
+
+	tofile, err := os.Create(toFile)
+	if err != nil {
+		return
+	}
+	defer tofile.Close()
+
+	return io.Copy(tofile, fromfile)
+}
+
