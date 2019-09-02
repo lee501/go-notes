329a88508677c62e3d4cb1a9bf61cb71b792a440
lee <yeluowoxin@sina.com>
2019-09-02 18:16:41 +0800

    unsafe pointer

--- struct/anonymous_struct.go
+++ struct/anonymous_struct.go
@@ -7,17 +7,15 @@
 // 那么这个结构体所拥有的全部字段都被隐式地引入了当前定义的这个结构体，
 package main
 
-import "go/ast"
-
 //人
-type Person struct {
+type Person1 struct {
 	name string
 	sex  byte
 	age  int
 }
 //学生
-type Student struct {
-	Person // 匿名字段，那么默认Student就包含了Person的所有字段
+type Student2 struct {
+	Person1 // 匿名字段，那么默认Student就包含了Person的所有字段
 	id     int
 	addr   string
 	name string // 同名字段
@@ -33,31 +31,31 @@ type Teacher struct {
 
 //结构体指针
 //注意成员使用.访问的时候，go是转换成指针后访问的： stu.name 转换为(*stu).name
-type Student1 struct {
-	*Person
+type Student3 struct {
+	*Person1
 	id int
 }
 func main() {
 	//初始化
-	s1 := Student{Person{"mike", 'm', 18}, 1, "sz", "ss"} //顺序初始化，注意Person的初始化方法
+	s1 := Student2{Person1{"mike", 'm', 18}, 1, "sz", "ss"} //顺序初始化，注意Person的初始化方法
 
-	s2 := Student{Person: Person{name: "lee"}, id: 1}  //部分成员初识化
+	s2 := Student2{Person1: Person1{name: "lee"}, id: 1}  //部分成员初识化
 
 	//成员操作
-	var s3 Student
+	var s3 Student2
 	s3.name = "lee" //同名字段默认给外层成员赋值
-	s3.Person.name = "anne" //同名字段给内层成员赋值
+	s3.Person1.name = "anne" //同名字段给内层成员赋值
 	s3.sex = 0 //等同于s3.Person.sex
 	//Person为结构体，是一个匿名字段
-	s3.Person = Person{"lee", 1, 22}
+	s3.Person1 = Person1{"lee", 1, 22}
 
 	//非结构体匿名字段的赋值
 	t := Teacher{int: 1, mystr: "hello"}
 
 	//	结构体指针的初始化
-	s4 := Student1{&Person{"mike", 1, 18}, 1}
+	s4 := Student3{&Person1{"mike", 1, 18}, 1}
 	// 声明变量方式
-	var s5 Student1
-	s5.Person = new(Person) //new 一个空间
-	s5.Person.name = "lee" //等同于s5.name
+	var s5 Student3
+	s5.Person1 = new(Person1) //new 一个空间
+	s5.Person1.name = "lee" //等同于s5.name
 }
--- struct/unsafe_pointer.go
+++ struct/unsafe_pointer.go
@@ -19,33 +19,33 @@ import (
 //	s = "456"
 //}
 /*
-首先，要理解go的string类型是什么
+		首先，要理解go的string类型是什么
 
-通过reflect包，我们可以知道，在Golang底层，string其实是struct：
+		通过reflect包，我们可以知道，在Golang底层，string其实是struct：
 
-type StringHeader struct { Data uintptr Len int }
+		type StringHeader struct { Data uintptr Len int }
 
-其实一个string是一个保存了真实字符数组指针和长度的结构体
+		其实一个string是一个保存了真实字符数组指针和长度的结构体
 
-对于fun1 ， a是一个int变量，unsafe.Pointer(&a) 是一个原生指针，指向的内存保存了一个int
+		对于fun1 ， a是一个int变量，unsafe.Pointer(&a) 是一个原生指针，指向的内存保存了一个int
 
-c := (*string)(unsafe.Pointer(&a)) 将这个原生指针转换为了指向string的指针
+		c := (*string)(unsafe.Pointer(&a)) 将这个原生指针转换为了指向string的指针
 
-注意c 现在是指向string的指针，换句话说，c指向的内存被当做了一个StringHeader 结构体来处理
+		注意c 现在是指向string的指针，换句话说，c指向的内存被当做了一个StringHeader 结构体来处理
 
-但是c指向的内存，只分配了一个int的长度，并且这个int的值是2
+		但是c指向的内存，只分配了一个int的长度，并且这个int的值是2
 
-所以目前c中 Data 值为2， Len是无效的，因为Len现在已经溢出了有效内存范围，其内的值是随机的(该溢出区域内存的原始值)
+		所以目前c中 Data 值为2， Len是无效的，因为Len现在已经溢出了有效内存范围，其内的值是随机的(该溢出区域内存的原始值)
 
-这时对 *c 进行赋值 "44", "44"是一个字符串常量，保存在只读的常量存储区中。
+		这时对 *c 进行赋值 "44", "44"是一个字符串常量，保存在只读的常量存储区中。
 
-赋值时 其实是把常量字符串"44"的指针赋给了c的Data字段，把其长度2赋给了c的Len字段。 注意这里是一个野指针赋值，因为 Len 字段已经溢出到有效内存之外了，但是这里panic的概率很低，因为内存padding机制
+		赋值时 其实是把常量字符串"44"的指针赋给了c的Data字段，把其长度2赋给了c的Len字段。 注意这里是一个野指针赋值，因为 Len 字段已经溢出到有效内存之外了，但是这里panic的概率很低，因为内存padding机制
 
-然后输出 fmt.Println(*c) 这里，因为c的Len字段是溢出到无效内存中，所以如果其他指令有变量定义的操作，很可能把Len字段给覆盖掉 一旦Len被覆盖，那么其中保存的长度就不在是2，这时输出c，很大概率就会panic。 如果运气好，Len没有被覆盖，那么输出c，就能正常输出44
+		然后输出 fmt.Println(*c) 这里，因为c的Len字段是溢出到无效内存中，所以如果其他指令有变量定义的操作，很可能把Len字段给覆盖掉 一旦Len被覆盖，那么其中保存的长度就不在是2，这时输出c，很大概率就会panic。 如果运气好，Len没有被覆盖，那么输出c，就能正常输出44
 
-对于fun2，可以根据上述原理自行分析。因为a变量也是一个StringHeader，c其实与unsafe.Pointer(&a)指向同一个StringHeader。
+		对于fun2，可以根据上述原理自行分析。因为a变量也是一个StringHeader，c其实与unsafe.Pointer(&a)指向同一个StringHeader。
 
-对于fun3, 因为c本来就是一个新创建的string变量，也就是一个独立的StringHeader，没有复用变量a的内存，所以是正常的赋值和输出
+		对于fun3, 因为c本来就是一个新创建的string变量，也就是一个独立的StringHeader，没有复用变量a的内存，所以是正常的赋值和输出
 */
 func main(){
 	//num := 5
@@ -56,16 +56,23 @@ func main(){
 	fun1()
 	fun2()
 	fun3()
-	var m *(*string)
-	fmt.Printf("%p", m)
-	fmt.Printf("%p", *m)
-
+	//var m *(*string)
+	//fmt.Printf("%p", m)
+	//fmt.Printf("%p", *m)
+	//var s *string
+	//s =
+	//fmt.Printf("%#v", s)
+	a := 1
+	b := a
+	var c *int
+	c = &a
+	fmt.Printf("%#v, %#v, %#v", a, b, *c)
 }
 
 func fun1()  {
-	a := "2"
-	c := (*int) (unsafe.Pointer(&a))
-	*c = 2
+	a := 2
+	c := (*string) (unsafe.Pointer(&a))
+	*c = "2222"
 	fmt.Println(*c)
 }
 
