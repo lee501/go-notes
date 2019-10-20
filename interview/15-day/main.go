7f55995fcf23e766c1eeb920fd51bfda2b28177f
lee <yeluowoxin@sina.com>
2019-10-21 01:15:06 +0800

    new a slice for delete element

--- interview/15-day/main.go
+++ interview/15-day/main.go
@@ -1,4 +1,4 @@
-package _5_day
+package main
 
 import (
 	"fmt"
@@ -17,12 +17,15 @@ func main() {
 	begin := time.Now()
 	s = append(s[:1], s[2:]...)
 	end := time.Now()
-	fmt.Println(end.UnixNano() - begin.UnixNano())
+	fmt.Println(end.UnixNano() - begin.UnixNano()) //0
 	m := []string{"a", "b", "c", "d"}
 	begin = time.Now()
-	re := make([]string, len(m)-1)
-	re = append(re, m[:1]...)
-	re = append(re, m[2:]...)
+	var s2 []string
+	for _, v := range m {
+		if v != "b" {
+			s2 = append(s2, v)
+		}
+	}
 	end = time.Now()
-	fmt.Println(end.UnixNano() - begin.UnixNano())
+	fmt.Println(end.UnixNano() - begin.UnixNano()) //1000~ 7000
 }
