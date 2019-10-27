b7c7cefdcaa201f677d5f52fcd82b064921339a0
lee <yeluowoxin@sina.com>
2019-10-27 22:33:43 +0800

    map init

--- golang-knowledge-point/add-string-method-for-enum.go
+++ golang-knowledge-point/add-string-method-for-enum.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type State int
 
 const (
@@ -10,16 +12,22 @@ const (
 )
 
 func (s State) String() string {
-	switch s {
-	case Running:
-		return "Runnning"
-	case Rebooting:
-		return "Rebooting"
-	case Stopped:
-		return "Stopped"
-	case Terminated:
-		return "Terminated"
-	default:
-		return "unknown"
-	}
+	//switch s {
+	//case Running:
+	//	return "Runnning"
+	//case Rebooting:
+	//	return "Rebooting"
+	//case Stopped:
+	//	return "Stopped"
+	//case Terminated:
+	//	return "Terminated"
+	//default:
+	//	return "unknown"
+	//}
+	return []string{"Running", "Stopped", "Rebooting", "Terminated"}[s]
+
+}
+
+func main() {
+	fmt.Println(Running)
 }
--- golang-knowledge-point/init-map.go
+++ golang-knowledge-point/init-map.go
@@ -1,5 +1,22 @@
 package main
 
+/*map的声明和初始化*/
+
+type People struct {
+	Name string
+}
+
 func main() {
-	
+	//1声明， 需要分配空间
+	var peopleDB map[string]People
+	peopleDB = make(map[string]People)
+	peopleDB["1234"] = People{"lee"}
+	//map的创建
+	//使用make
+	test := make(map[string]People)
+	test["222"] = People{"test"}
+	//创建并初始化
+	peopleDB = map[string]People{
+		"123": People{"anne"},
+	}
 }
