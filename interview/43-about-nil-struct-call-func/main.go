627349027eedf0aeaf8f2616c46f44973d4e554b
lee <yeluowoxin@sina.com>
2020-01-11 22:40:23 +0800

    53 chapter

---
+++ interview/43-about-nil-struct-call-func/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+/*
+	struct方法中，指针类型的接收者必须是合法指针（包括 nil）,或能获取实例地址
+*/
+type X struct {
+
+}
+
+func (x *X) callmethod() {
+	fmt.Println("test")
+}
+
+func main() {
+	//nil是合法的调用
+	var x *X
+	x.callmethod()
+	//cannot take the address of X literal, X{}是不可寻址的
+	//X{}.callmethod()
+	//	正确处理
+	a := X{}
+	a.callmethod()
+
+	findItemInMap()
+}
+
+func findItemInMap() {
+	x := map[string]string{"one":"a","two":"","three":"c"}
+	v := x["one"]
+	fmt.Println(v)
+	//检查map是否存在值，使用map返回的第二个参数
+	if _, ok := x["once"]; !ok {
+		fmt.Println("no once")
+	}
+}
