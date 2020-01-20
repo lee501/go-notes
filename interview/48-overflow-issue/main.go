38377d0a9bbe44c84e2e21b8aceb75bf16e484f0
lee <yeluowoxin@sina.com>
2020-01-20 21:57:58 +0800

    63 chapter over nil issue

---
+++ interview/48-overflow-issue/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+/*
+	int8 取值范围 -128～127
+*/
+
+func main() {
+	var x int8 = 128 // constant 128 overflows int8
+	y := x/-1
+	fmt.Println(y)
+}
+
+/*
+	go 预定义的nil可以被覆盖
+*/
+
+func overNil() {
+	nil := 123
+	fmt.Println(nil)
+	//var _ map[string]int = nil 这里nil被覆盖为int类型， 不能赋值给map
+}
