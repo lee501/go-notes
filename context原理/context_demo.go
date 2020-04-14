1559ae1bc6c6e2d008cad04103dd74ecc900078f
lee <yeluowoxin@sina.com>
2020-04-14 16:25:50 +0800

    注释

--- context原理/context_demo.go
+++ context原理/context_demo.go
@@ -8,11 +8,14 @@ import (
 func main() {
 	ctxa, cancel := context.WithCancel(context.Background())
 	go work(ctxa, "work1")
+	//返回一个context，设置超时时间为3秒
 	ctxb, _ := context.WithTimeout(ctxa, time.Second*3)
 	go work(ctxb, "work2")
+	//当work2超时关闭的时候，通知work3 quit
 	ctxc := context.WithValue(ctxb, "key", "custom value")
 	go workWithValue(ctxc, "work3")
 	time.Sleep(5*time.Second)
+	//手动关闭work1
 	cancel()
 	time.Sleep(time.Second)
 }
