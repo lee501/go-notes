0b9c7d6c038c73ab4cd93fc35cada5dcb0dde0c6
lee <yeluowoxin@sina.com>
2021-03-04 08:19:07 +0800

    map value is struct赋值问题

--- map值为struct/map-value-is-struct.go
+++ map值为struct/map-value-is-struct.go
@@ -1,4 +1,4 @@
-package issue
+package main
 
 import "fmt"
 
@@ -12,16 +12,22 @@ type P struct {
 	sex string
 }
 
-func issue() {
+func main() {
 	m := make(map[string]P)
 	//结构体不能直接赋值字段
 	//m["a"].name = "lee"
 	m["a"] = P{name: "lee", sex: "man"}
-	fmt.Println(m)
+	//直接初始化
+	t := map[string]P{"key": P{name: "lee", sex: "nan"}}
+	//赋值value 为temp, temp可寻址，此时可以更改temp的属性name和sex
+	temp := t["key"]
+	temp.name = "anne"
+	//更改后的t中value不变， （struct为值类型）
+	fmt.Println(t, temp)
 	//当使用指针的时候可以直接赋值
 	n := make(map[string]*P)
 	p := &P{"lee", "man"}
 	n["a"] = p
 	n["a"].name = "test"
-	fmt.Println(n["a"].name)
+	fmt.Println(n["a"])
 }
