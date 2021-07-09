9028a87fb715d1df66b6c5268cd214bdbf4fc24d
lee <yeluowoxin@sina.com>
2021-07-09 16:09:50 +0800

    reflect func example

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/golang/protobuf v1.5.1 // indirect
+	github.com/googollee/go-socket.io v1.6.0 // indirect
 	github.com/gorilla/websocket v1.4.2
 	github.com/patrickmn/go-cache v2.1.0+incompatible
 	go.uber.org/zap v1.16.0
--- golang-knowledge-point/add-string-method-for-enum.go
+++ golang-knowledge-point/add-string-method-for-enum.go
@@ -32,24 +32,26 @@ func main() {
 	fmt.Println(Running)
 	r := test()
 	if r == KB {
-		fmt.Println("kb")
+		fmt.Println(r)
 	}
+	fmt.Println(r)
 }
 
 //kb mb gb定义
 type ByteSize float64
 
 const (
-	_           = iota                   // ignore first value by assigning to blank identifier
+	_           = iota             // ignore first value by assigning to blank identifier
 	KB ByteSize = 1 << (10 * iota) // 1 << (10*1)
-	MB                                   // 1 << (10*2)
-	GB                                   // 1 << (10*3)
-	TB                                   // 1 << (10*4)
-	PB                                   // 1 << (10*5)
-	EB                                   // 1 << (10*6)
-	ZB                                   // 1 << (10*7)
-	YB                                   // 1 << (10*8)
+	MB                             // 1 << (10*2)
+	GB                             // 1 << (10*3)
+	TB                             // 1 << (10*4)
+	PB                             // 1 << (10*5)
+	EB                             // 1 << (10*6)
+	ZB                             // 1 << (10*7)
+	YB                             // 1 << (10*8)
 )
+
 func test() ByteSize {
-	return KB
+	return MB
 }
