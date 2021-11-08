6682fda49716c5b78ceede9dddf47b19ffc4da80
lee <yeluowoxin@sina.com>
2021-11-09 00:01:50 +0800

    golang-gc

--- leetcode/heapsort.go
+++ leetcode/heapsort.go
@@ -8,10 +8,19 @@ import "fmt"
 		父节点i的左右子节点 2*i+1、2*i+2
 */
 //构建堆 [4,6,8,5,9]
+/*
+4        4 		  9		  9
+/\       /\		  /\	  /\
+6 8 ->  9  8 ->  4  8    6  8 (构建大根堆) -> swap 首尾元素，重新构建剩下元素   8		5       6
+/\      /\       /\      /\												   /\		/\		/\
+5 9    5  6      5 6    5  4											  6   4 -> 6  4 -> 5  4
+																		 /
+																		5
+*/
 //len数组长度， i为最后一个非叶节点8 （len/2 -1）
 func AdjustHeap(array []int, len, i int) {
 	root := array[i]
-	for l := 2*i+1; l<len; l=l*2+1 {
+	for l := 2*i + 1; l < len; l = l*2 + 1 {
 		if l+1 < len && array[l] < array[l+1] {
 			l++
 		}
@@ -26,11 +35,11 @@ func AdjustHeap(array []int, len, i int) {
 func HeapSort(array []int) {
 	len := len(array)
 	//从最后一个非叶子节点从下到上，从右到左调整
-	for i:=len/2-1;i>=0;i--{
+	for i := len/2 - 1; i >= 0; i-- {
 		AdjustHeap(array, len, i)
 	}
 	//交换堆顶元素与末尾元素
-	for j:=len-1; j>0; j-- {
+	for j := len - 1; j > 0; j-- {
 		swap(array, 0, j)
 
 		AdjustHeap(array, j, 0)
@@ -42,7 +51,7 @@ func swap(array []int, i, j int) {
 }
 
 func main() {
-	arr := []int{4,6,8,5,9}
+	arr := []int{4, 6, 8, 5, 9}
 	HeapSort(arr)
 	fmt.Println(arr)
 }
