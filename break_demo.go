ded1379a3bc0c33e287b7ce63fbb9187dc2c1bb0
lee <yeluowoxin@sina.com>
2021-12-01 17:37:41 +0800

    break to cycle

---
+++ break_demo.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	ticker := time.NewTicker(time.Second * 2)
+	timer := time.NewTimer(time.Second * 9)
+cycle:
+	for {
+		select {
+		case <-ticker.C:
+			fmt.Println("*********ticker  break********")
+		case <-timer.C:
+			fmt.Println("break exit")
+			break cycle
+		}
+	}
+
+	ticker.Stop()
+	timer.Stop()
+	fmt.Println("break test over!")
+}
