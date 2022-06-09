7da7034368c76ade3399dfe12cdad4c304173dfb
lee <261169185@qq.com>
2022-06-09 10:27:32 +0800

    sync pool

--- go-lib/sync.Map源码.go
+++ go-lib/sync.Map源码.go
@@ -29,6 +29,6 @@ read 和 dirty 各自维护一套 key，key 指向的都是同一个 value。也
 mu: 保护read 和dirty
 read: 只读数据，指出并发读取 (atomic.Value 类型) 。如果需要更新 read，需要加锁保护数据安全。
 read 实际存储的是 readOnly 结构体，内部是一个原生 map，amended 属性用于标记 read 和 dirty 的数据是否一致
-dirty: 读写数据，非线性安全的原生 map。包含新写入的 key，并且包含 read 中所有未被删除的 key
+dirty: 读写数据，非线性安全的原生 map。包含新sy写入的 key，并且包含 read 中所有未被删除的 key
 misses: 统计有多少次读取 read 没有被命中。每次 read 读取失败后，misses 的计数加 1
 */
