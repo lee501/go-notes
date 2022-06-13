c996d52498d2d85d853ff2fa0f26162b9b69e3ba
lee <261169185@qq.com>
2022-06-13 13:06:45 +0800

    feat: lru algrithm

--- lru/lru.go
+++ lru/lru.go
@@ -1,10 +1,15 @@
 package lru
 
 import (
-	"bufio"
 	"container/list"
-	"net"
 )
+
+/*
+	LRU: 最近最少使用，核心思想是(如果数据最近被访问过，那么将来被访问的几率也更高)
+		1. 新数据插入到链表头部；
+		2. 每当缓存命中（即缓存数据被访问），则将数据移到链表头部；
+		3. 当链表满的时候，将链表尾部的数据丢弃。
+*/
 
 type LRUCache struct {
 	capacity int
@@ -12,19 +17,38 @@ type LRUCache struct {
 	list     *list.List
 }
 
-func NewList() *list.List {
-	return list.New()
+type Pair struct {
+	key   int
+	value int
 }
 
-type Packet struct {
-
+func Constructor(capacity int) LRUCache {
+	return LRUCache{
+		capacity: capacity,
+		cache:    make(map[int]*list.Element),
+		list:     list.New(),
+	}
+}
+func (lc *LRUCache) Get(key int) int {
+	if elem, ok := lc.cache[key]; ok {
+		lc.list.MoveToFront(elem)
+		return elem.Value.(Pair).value
+	}
+	return -1
 }
 
-type Channel struct {
-	conn net.Conn    // WebSocket 连接
-	send chan Packet
-}
-
-func (c *Channel) reader() {
-	buf := bufio.NewReader(c.conn)
+func (lc *LRUCache) Put(key, value int) {
+	//hit cache, remove the elem to list head
+	if elem, ok := lc.cache[key]; ok {
+		elem.Value = Pair{key, value}
+		lc.list.MoveToFront(elem)
+	} else {
+		//del cache and last elem when list full
+		if lc.list.Len() >= lc.capacity {
+			delete(lc.cache, lc.list.Back().Value.(Pair).key)
+			lc.list.Remove(lc.list.Back())
+		}
+		lc.list.PushFront(Pair{key, value})
+		lc.cache[key] = lc.list.Front()
+	}
 }
--- lru/lru_test.go
+++ lru/lru_test.go
@@ -1,11 +1,1 @@
 package lru
-
-import (
-	"fmt"
-	"testing"
-)
-
-func TestNewList(t *testing.T) {
-	l := NewList()
-	fmt.Println(l.Front())
-}
