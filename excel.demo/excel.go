dd0dd2476601dd168e77576031cfd831ba080faf
lee <261169185@qq.com>
2022-08-09 17:53:29 +0800

    excel add value

--- excel.demo/excel.go
+++ excel.demo/excel.go
@@ -7,46 +7,48 @@ import (
 )
 
 func main() {
-	categories := map[string]string{
-		"A2": "Small", "A3": "Normal", "A4": "Large",
-		"B1": "Apple", "C1": "Orange", "D1": "Pear"}
-	values := map[string]int{
-		"B2": 2, "C2": 3, "D2": 3, "B3": 5, "C3": 2, "D3": 4, "B4": 6, "C4": 7, "D4": 8}
-	f := excelize.NewFile()
-	for k, v := range categories {
-		f.SetCellValue("Sheet1", k, v)
-	}
+	values := map[string]interface{}{
+		"C2": "北京", "D2": 8, "E2": 10, "F2": 20}
+	f, _ := excelize.OpenFile("/Users/lee/Downloads/report_template.xlsx")
+	defer func() {
+		f.Close()
+	}()
 	for k, v := range values {
-		f.SetCellValue("Sheet1", k, v)
+		err := f.SetCellValue("index", k, v)
+		fmt.Println(err, k)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
 	}
-	if err := f.AddChart("Sheet1", "E1", `{
-        "type": "colStacked",
-        "series": [
-        {
-            "name": "Sheet1!$A$2",
-            "categories": "Sheet1!$B$1:$D$1",
-            "values": "Sheet1!$B$2:$D$2"
-        },
-        {
-            "name": "Sheet1!$A$3",
-            "categories": "Sheet1!$B$1:$D$1",
-            "values": "Sheet1!$B$3:$D$3"
-        },
-        {
-            "name": "Sheet1!$A$4",
-            "categories": "Sheet1!$B$1:$D$1",
-            "values": "Sheet1!$B$4:$D$4"
-        }],
-        "title":
-        {
-            "name": "Fruit 3D Clustered Column Chart"
-        }
-    }`); err != nil {
-		fmt.Println(err)
-		return
-	}
-	// Save spreadsheet by the given path.
-	if err := f.SaveAs("Book1.xlsx"); err != nil {
+	//if err := f.AddChart("Sheet1", "E1", `{
+	//   "type": "colStacked",
+	//   "series": [
+	//   {
+	//       "name": "Sheet1!$A$2",
+	//       "categories": "Sheet1!$B$1:$D$1",
+	//       "values": "Sheet1!$B$2:$D$2"
+	//   },
+	//   {
+	//       "name": "Sheet1!$A$3",
+	//       "categories": "Sheet1!$B$1:$D$1",
+	//       "values": "Sheet1!$B$3:$D$3"
+	//   },
+	//   {
+	//       "name": "Sheet1!$A$4",
+	//       "categories": "Sheet1!$B$1:$D$1",
+	//       "values": "Sheet1!$B$4:$D$4"
+	//   }],
+	//   "title":
+	//   {
+	//       "name": "Fruit 3D Clustered Column Chart"
+	//   }
+	//}`); err != nil {
+	//	fmt.Println(err)
+	//	return
+	//}
+	//// Save spreadsheet by the given path.
+	if err := f.SaveAs("demo.xlsx"); err != nil {
 		fmt.Println(err)
 	}
 }
