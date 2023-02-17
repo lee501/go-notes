5ca451959302756231cc877cf34ea2eccdc61ff5
lee501 <261169185@qq.com>
2023-02-17 13:09:17 +0800

    add flag path

--- go-web/static-app/main.go
+++ go-web/static-app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"html/template"
 	"net/http"
@@ -12,7 +13,10 @@ type User struct {
 }
 
 func welcome(w http.ResponseWriter, r *http.Request) {
-	t, _ := template.ParseFiles("view/index.html")
+	t, err := template.ParseFiles("view/index.html")
+	if err != nil {
+		fmt.Println(err)
+	}
 	//t.Execute(w, "lee") //第二个参数表示向模版传递的数据
 	//传递结构体
 	//t.Execute(w, User{"lee", 20} )
@@ -24,22 +28,50 @@ func welcome(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	//usage go run main.go -path="/Users/lee/workspace/gp-notes/go-web/static-app/static"
+	var path string
+	flag.StringVar(&path, "path", "/static/", "static path")
+	flag.Parse()
 	server := http.Server{Addr: ":8899"}
-	fmt.Println("服务启动")
 	/*
 		访问url以/static/开头,就会把访问信息映射到指定的目录中
 	*/
 	//http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-	http.HandleFunc("/", welcome)
-	http.HandleFunc("/", funcParam)
-	http.HandleFunc("/", nest) //template
+	http.HandleFunc("/wel", welcome)
+	//http.HandleFunc("/param", funcParam)
+	http.HandleFunc("/nest", nest) //template
 
-	http.HandleFunc("/", index)
+	http.HandleFunc("/index", index)
 	http.HandleFunc("/setCookie", setCookie)
 	http.HandleFunc("/getCookie", getCookie)
 	err := server.ListenAndServe()
+	fmt.Println("服务启动")
 	if err != nil {
 		fmt.Println(err)
 	}
+}
+
+func index(w http.ResponseWriter, re *http.Request) {
+	t, _ := template.ParseFiles("view/cookie.html")
+	t.Execute(w, nil)
+}
+
+func getCookie(w http.ResponseWriter, re *http.Request) {
+	//c, _ := re.Cookie("name")
+	c := re.Cookies()
+	t, _ := template.ParseFiles("view/cookie.html")
+	t.Execute(w, c)
+}
+
+func setCookie(w http.ResponseWriter, re *http.Request) {
+	c := http.Cookie{Name: "name", Value: "lee"}
+	http.SetCookie(w, &c)
+	t, _ := template.ParseFiles("view/cookie.html")
+	t.Execute(w, nil)
+}
+
+func nest(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("view/content.html", "view/header.html", "view/footer.html")
+	t.ExecuteTemplate(w, "content", nil)
 }
--- struct/main.go
+++ struct/main.go
@@ -1,15 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
 /*
 	声明为变量时，并分配内存空间, 在进行struct中的变量赋值时，可直接使用。
 	声明为指针时，并未进行赋值，也没有分配内存空间
 */
 var c Man
+
 type Man struct {
 	Name string
 }
+
 func main() {
 	var a *Man
 	//指针必须指定内存地址，否则//invalid memory address or nil pointer dereference
@@ -28,4 +34,13 @@ func main() {
 
 	c.Name = "lee"
 	fmt.Println()
+
+	s, _ := M()
+	fmt.Println(s)
+}
+
+func M() (string, error) {
+	b := []byte{'"', '{', '}', '"'}
+	str := string(b)
+	return strconv.Unquote(strings.Replace(strconv.Quote(str), `\\u`, `\u`, -1))
 }
